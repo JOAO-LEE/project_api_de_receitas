38609ba28cc87218c696772804b73558e501f9ca
João Lee <joaumlimaum@gmail.com>
2023-07-28 19:08:16 -0300

    Requisito 11 concluído

--- src/recipes-api/Controllers/CommentController.cs
+++ src/recipes-api/Controllers/CommentController.cs
@@ -32,6 +32,7 @@ public class CommentController : ControllerBase
   [HttpGet("{name}", Name = "GetComment")]
   public IActionResult Get(string name)
   {
-    throw new NotImplementedException();
+    var recipeComments = _service.GetComments(name);
+    return Ok(recipeComments);
   }
 }
