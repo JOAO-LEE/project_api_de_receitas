e4bbd90462385cdbb18f29a82bb97b335f0af5d4
João Lee <joaumlimaum@gmail.com>
2023-07-28 14:00:37 -0300

    Requisito 3 concluído

--- src/recipes-api/Controllers/RecipesController.cs
+++ src/recipes-api/Controllers/RecipesController.cs
@@ -42,7 +42,8 @@ public class RecipesController : ControllerBase
   [HttpPost]
   public IActionResult Create([FromBody] Recipe recipe)
   {
-    throw new NotImplementedException();
+    _service.AddRecipe(recipe);
+    return StatusCode(201, recipe);
   }
 
   // 4 - Sua aplicação deve ter o endpoint PUT /recipe
